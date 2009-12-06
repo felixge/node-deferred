6f26373265e126eda8d1be8b3868a96b973a4d94
Felix Geisendörfer <felix@debuggable.com>
2009-12-06 22:01:13 +0100

    Implemented Deferred.trycatch
    I think automatically catching exceptions in Deferred callbacks is a bad
    idea. If you do not specificially address this case with an errback
    during development, you will never know there was a problem.
    
    My proposed solution is the herby introduced trycatch() method which
    will set the Deferred's autoCatch property to true which means it will
    behave exactly like dojo.Deferreds. I just don't think we should behave
    like them in general.

--- test/test-deferred.js
+++ test/test-deferred.js
@@ -1,13 +1,45 @@
 process.mixin(require('sys'));
+var assert = require('assert');
+
 var Deferred = require('../lib/deferred').Deferred;
 
-var d = new Deferred();
-d
+var a = new Deferred();
+a
   .addCallback(function(result) {
     return result+' world';
   })
   .addCallback(function(result) {
-    p(result);
+    assert.equal('hello world', result);
   });
+a.callback('hello');
 
-d.callback('hello');
+// This is different from dojo.Deferred, by default do not try..catch errors
+var b = new Deferred();
+b
+  .addCallback(function() {
+    throw new Error("Do not catch me");
+  })
+  .addErrback(function() {
+    assert.ok(false, "Unreachable");
+  });
+assert.throws(function() {
+ b.callback();
+});
+
+// However, we can explicitely turn on try..catch if we need it
+var b = new Deferred();
+var didCatch = false;
+b
+  .trycatch()
+  .addCallback(function() {
+    throw new Error("catchme");
+  })
+  .addErrback(function(e) {
+    assert.equal("catchme", e.message);
+    didCatch = true;
+  });
+b.callback();
+
+process.addListener('exit', function() {
+  assert.ok(didCatch);
+});
